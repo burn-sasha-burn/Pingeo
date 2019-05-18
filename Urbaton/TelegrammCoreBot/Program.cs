26ea4187f8aca00dad73e83aadfbc0cede07c58c
Georgy Voschuk <voschuk@skbkontur.ru>
2019-05-18 16:41:05 +0400

    Подробный вывод ошибок

--- Urbaton/TelegrammCoreBot/Program.cs
+++ Urbaton/TelegrammCoreBot/Program.cs
@@ -19,6 +19,8 @@ namespace TelegrammCoreBot
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .CaptureStartupErrors(true) // the default
+                .UseSetting("detailedErrors", "true")
                 .UseStartup<Startup>();
     }
 }
