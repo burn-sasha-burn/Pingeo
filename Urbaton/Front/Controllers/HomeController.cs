250d4b5236630efb26e2cc9acc8d76f789f150e9
zhelonkin <Zhelonkin.ru@gmail.com>
2019-05-18 11:07:34 +0400

    Front-бэк-проект

---
+++ Urbaton/Front/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace Front.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Urbaton/Front/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using log4net.Config;
+
+namespace Front
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            XmlConfigurator.Configure();
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
