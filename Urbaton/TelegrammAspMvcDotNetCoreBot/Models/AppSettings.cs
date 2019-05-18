6174e23480e6166507798708b98d6bb70413cb1c
budaev <budaev@skbkontur.ru>
2019-05-18 13:32:15 +0400

    Заготовка бота

---
+++ Urbaton/TelegrammAspMvcDotNetCoreBot/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegrammAspMvcDotNetCoreBot.Models
+{
+    public class AppSettings
+    {
+        public static string Url { get; set; } = "https://a3864fab.ngrok.io/{0}";
+        public static string Name { get; set; } = "Urbot";
+        public static string Key { get; set; } = "";
+    }
+}
