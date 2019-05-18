dc67e39b376c3843835ff6e0450c8a5d8255975b
miroshnikov.d <miroshnikov.d@skbkontur.ru>
2019-05-19 03:11:45 +0400

    Картиночки

--- Urbaton/Front/Content/components/IncidentInfo/IncidentInfo.tsx
+++ Urbaton/Front/Content/components/IncidentInfo/IncidentInfo.tsx
@@ -1,5 +1,6 @@
 import CrossIcon from '@skbkontur/react-icons/Delete';
 import {Coordinate} from 'components/Coordinate/Coordinate';
+import {IncidentImages} from 'components/IncidentImages/IncidentImages';
 import {IncidentStatus} from 'components/IncidentStatus/IncidentStatus';
 import {IIncident} from 'domain/IIncident';
 import {IPoint} from 'domain/IPoint';
@@ -28,21 +29,24 @@ export function IncidentInfo({incident, onClose, onToMap}: IIncidentInfoProps) {
                     <CrossIcon/>
                 </button>
             </div>
-            <IncidentStatus status={incident.status}/>
-            <p>
-                <span>Место:</span>{' '}
-                <Hint text="Перейти к карте">
-                    <Button use="link" onClick={() => onToMap(incident.location)}>
-                        <Coordinate {...incident.location}/>
-                    </Button>
-                </Hint>
-            </p>
-            <p>
-                Описание: {incident.description}
-            </p>
-            <p>
-                customText: {incident.customText}
-            </p>
+            <div className={styles.incidentInfo}>
+                <IncidentStatus status={incident.status}/>
+                <p>
+                    <span>Место:</span>{' '}
+                    <Hint text="Перейти к карте">
+                        <Button use="link" onClick={() => onToMap(incident.location)}>
+                            <Coordinate {...incident.location}/>
+                        </Button>
+                    </Hint>
+                </p>
+                <p>
+                    Описание: {incident.description}
+                </p>
+                <p>
+                    customText: {incident.customText}
+                </p>
+                <IncidentImages images={incident.images}/>
+            </div>
         </div>
     );
 }
--- Urbaton/Front/Content/domain/IImage.ts
+++ Urbaton/Front/Content/domain/IImage.ts
@@ -1,3 +1,4 @@
 export interface IImage {
     someValue: string;
+    link: string;
 }
