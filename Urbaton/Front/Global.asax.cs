09d94f8b5c8c16d379559de38a098cb126a39e89
zhelonkin <Zhelonkin.ru@gmail.com>
2019-05-18 13:18:46 +0400

    Починил лог

--- Urbaton/Front/Global.asax.cs
+++ Urbaton/Front/Global.asax.cs
@@ -1,7 +1,6 @@
 ﻿using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
-using log4net.Config;
 
 namespace Front
 {
@@ -9,7 +8,6 @@ namespace Front
     {
         protected void Application_Start()
         {
-            XmlConfigurator.Configure();
             IocFactory.Configure();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
