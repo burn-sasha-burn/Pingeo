92a7357105ab0ca256b6422e6f4039e8c9c98bf1
miroshnikov.d <miroshnikov.d@skbkontur.ru>
2019-05-19 12:11:50 +0400

    Генерация инцидентов в доплениение к выгрузке

--- Urbaton/Front/Content/store/actions/incidentsActions.ts
+++ Urbaton/Front/Content/store/actions/incidentsActions.ts
@@ -1,5 +1,7 @@
 import {getIncidents} from 'api/incidentsApi';
 import {IIncident} from 'domain/IIncident';
+import {IPoint} from 'domain/IPoint';
+import {IStatus} from 'domain/IStatus';
 import {GeneralGetState, GeneralThunkAction, GeneralThunkDispatch} from 'utils/store/actionTypes';
 import {createByIdActions} from 'utils/store/createByIdActions';
 
@@ -18,4 +20,25 @@ export function fetchAllIncidents(): GeneralThunkAction<Promise<IIncident[]>> {
                 return incidents;
             });
     };
+}
+
+export function generateIncidents(point: IPoint) {
+    const incidents: IIncident[] = [];
+    for (let i = 0; i < 50; i++) {
+        incidents.push({
+            id: Math.random().toString(36).slice(2),
+            status: Math.random() < 0.33 ? IStatus.Finished : Math.random() > 0.5 ? IStatus.New : IStatus.Process,
+            creationDate: new Date().toISOString(),
+            creator: {nick: 'noone'},
+            customText: 'asdf',
+            image: {fileId: 'AgADAgADQKoxGzO8CUutQ9XN2l_gbc49hA8ABMtu0GGOnqcezNwAAgI'},
+            location: {
+                lat: point.lat + (Math.random() - 0.5) * 0.02 / 2,
+                lng: point.lng + (Math.random() - 0.5) * 0.02,
+            },
+            meetupUsers: [],
+            description: `Сгенерированный инцидент ${i + 1}`,
+        });
+    }
+    return saveIncidents(incidents);
 }
