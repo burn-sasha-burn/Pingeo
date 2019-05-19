cf9e816830e0233c6646c65c64b1d724383945e9
miroshnikov.d <miroshnikov.d@skbkontur.ru>
2019-05-19 12:48:05 +0400

    Убрал фейковые инциденты

--- Urbaton/Front/Content/store/actions/incidentsActions.ts
+++ Urbaton/Front/Content/store/actions/incidentsActions.ts
@@ -24,22 +24,22 @@ export function fetchAllIncidents(): GeneralThunkAction<Promise<IIncident[]>> {
 
 export function generateIncidents(point: IPoint) {
     const incidents: IIncident[] = [];
-    for (let i = 0; i < 50; i++) {
-        incidents.push({
-            id: Math.random().toString(36).slice(2),
-            status: Math.random() < 0.33 ? IStatus.Finished : Math.random() > 0.5 ? IStatus.New : IStatus.Process,
-            creationDate: new Date().toISOString(),
-            creator: {nick: 'noone'},
-            customText: 'asdf',
-            fileId: 'AgADAgADQKoxGzO8CUutQ9XN2l_gbc49hA8ABMtu0GGOnqcezNwAAgI',
-            location: {
-                lat: point.lat + (Math.random() - 0.5) * 0.02 / 2,
-                lng: point.lng + (Math.random() - 0.5) * 0.02,
-            },
-            meetupUsers: [],
-            description: `Сгенерированный инцидент ${i + 1}`,
-            situation: ``,
-        });
-    }
+    // for (let i = 0; i < 50; i++) {
+    // incidents.push({
+    //     id: Math.random().toString(36).slice(2),
+    //     status: Math.random() < 0.33 ? IStatus.Finished : Math.random() > 0.5 ? IStatus.New : IStatus.Process,
+    //     creationDate: new Date().toISOString(),
+    //     creator: {nick: 'noone'},
+    //     customText: 'asdf',
+    //     fileId: 'AgADAgADQKoxGzO8CUutQ9XN2l_gbc49hA8ABMtu0GGOnqcezNwAAgI',
+    //     location: {
+    //         lat: point.lat + (Math.random() - 0.5) * 0.02 / 2,
+    //         lng: point.lng + (Math.random() - 0.5) * 0.02,
+    //     },
+    //     meetupUsers: [],
+    //     description: `Сгенерированный инцидент ${i + 1}`,
+    //     situation: ``,
+    // });
+    // }
     return saveIncidents(incidents);
 }
